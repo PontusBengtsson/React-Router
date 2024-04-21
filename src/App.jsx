6cb059d3f99be7149f8da068982a47217654c42d
PontusBengtsson <pontus.bengtsson@live.com>
2024-04-21 23:31:41 +0200

    Profiles

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,32 @@ import {
 //Pages
 import About from './Pages/About'
 import Home from './Pages/Home'
+import Faq from './Pages/help/Faq'
+import Contact from './Pages/help/Contact'
+import NotFound from './Pages/help/NotFound'
+import Profile, { profileLoader } from './profiles/Profile'
 
 //Layouts
 import RootLayout from './layouts/RootLayout'
+import HelpLayout from './layouts/HelpLayout'
+import ProfileLayout from './layouts/ProfileLayout'
 
 const routesFromElements = createRoutesFromElements(
     <Route path="/" element ={<RootLayout />}>
         <Route index element ={<Home />}/>
         <Route path="about" element ={<About />}/>
+        <Route path='help' element= { <HelpLayout />}>
+            <Route path ="faq" element ={<Faq />}/>
+            <Route path ="contact" element ={<Contact />}/>
+        </Route>
+        <Route
+                path="profiles"
+                element={<ProfileLayout />}
+            >
+                <Route index element={<Profile />} loader={profileLoader} />
+            </Route>
+
+            <Route path="*" element={<NotFound />} />
     </Route>
 );
 
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,19 +1,31 @@
-import { Outlet, NavLink} from "react-router-dom"
+import { Outlet, NavLink, Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
+
 const RootLayout = () => {
     return (
-       <div className ="root-layout">
-        <header>
-            <nav className="navbar">
-                <img src="" alt="" className="logo" />
-                <div className="nav-links">
-                    <NavLink className="nav-link" to="/">Home</NavLink>
-                    <NavLink className="nav-link" to="about">Om oss</NavLink>
-                </div>
-            </nav>
-        </header>
-        <main>
-            <Outlet/>
-        </main>
+        <div className="root-layout">
+            <header>
+                <nav className="navbar">
+                    <img src={logo} alt="Techover-logo" className="logo" />
+                    <div className="nav-links">
+                    <NavLink className="nav-link" to="/">
+                            Hem
+                        </NavLink>
+                        <NavLink className="nav-link" to="about">
+                            Om oss
+                        </NavLink>
+                        <NavLink className="nav-link" to="help">
+                            Hjälp
+                        </NavLink>
+                        <NavLink className="nav-link" to="profiles">
+                            Profiler
+                        </NavLink>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                <Outlet />
+            </main>
         </div>
     )
 }
